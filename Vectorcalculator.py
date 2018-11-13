7fa7d03f1cdcc1b506baf7c93f60d534027f1132
smithbinah <bsmith@student.neumont.edu>
2018-11-12 21:38:51 -0700

    finished calculating unit vectors

--- Vectorcalculator.py
+++ Vectorcalculator.py
@@ -1,16 +1,32 @@
 import math
+from itertools import izip
 class Vector:
-    exponentArray = []
+    exponentDict = {}
+    unitVector = dict()
+    
     def __init__(self, XVector, YVector):
         self.Xvector = XVector
         self.Yvector = YVector
-        self.vector =  [self.Xvector, self.Yvector]
+        self.vector =  {self.Xvector: self.Yvector}
+        self.magnitude = self.get_magnitude()
+        self.unitVector=self.unit_vector()
 
-    def magnitude(self):
-        for coordinate in self.vector:
-            self.exponentArray.append(coordinate ** 2)
+
+    def get_magnitude(self):
+        for coordinate, coordinate1 in self.vector.items():
+            self.exponentDict[coordinate ** 2] = coordinate1 ** 2
+        for exponent1, exponent2 in self.exponentDict.items():
+            answer = math.sqrt(exponent1 + exponent2)
+        return answer
+    def unit_vector(self):
+        for x, y in self.vector.items():
+            answer = self.unitVector[str((1/self.magnitude) * x)] = (1/self.magnitude) * y
+            print((1/self.magnitude) * x)
+            print((1/self.magnitude) * y)
+        return answer
+
+                     
+            
 v1 = Vector(2,3)
-# print(v1.vector)
-v1.magnitude()
-print(v1.exponentArray)
-
+print(v1.magnitude)
+print(v1.unitVector)
